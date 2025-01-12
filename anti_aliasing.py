d538a531a1b1b363a99f0dd70cef6d6ef7dc6822
DaiFuGu <patipat.kd@gmail.com>
2025-01-12 14:24:56 +0700

    Initial commit

---
+++ anti_aliasing.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+
+a = cv2.imread('barbara.tif', cv2.IMREAD_GRAYSCALE)
+
+# Downscale
+b = cv2.resize(a, None, fx=0.25, fy=0.25, interpolation=cv2.INTER_LINEAR)
+
+cv2.imwrite('downscale_image.png', b)
+cv2.imshow('Downscale Image', b / np.max(b))
+
+# Upscale
+c = cv2.resize(b, None, fx=4, fy=4, interpolation=cv2.INTER_LINEAR)
+
+cv2.imwrite('upscale_image.png', c)
+cv2.imshow('Upscale Image', c / np.max(c))
+
+H = np.zeros((512, 512))
+H[256-64:256+64, 256-64:256+64] = 1
+
+# FFT
+Da = np.fft.fft2(a)
+Da = np.fft.fftshift(Da)
+Dd = Da * H
+
+# inverse FFT
+Dd = np.fft.ifftshift(Dd)
+d = np.real(np.fft.ifft2(Dd))
+
+# result
+cv2.imwrite('filtered_image.png', d)
+cv2.imshow('Filtered Image', d / np.max(d))  # Normalize for display
+cv2.waitKey(0)
+cv2.destroyAllWindows()
